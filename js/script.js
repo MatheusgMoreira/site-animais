1885739fda416356814f652a7bbdcdd5b06ea06e
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-03-16 12:53:01 -0300

    site-animais

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from './modules/scrollSuave.js';
+import initTabNav from './modules/tabNav.js';
+import initAccordion from './modules/accordion.js';
+import initAnimacaoScroll from './modules/animacaoScroll.js';
+import initModal from './modules/modal.js'
+import initTolltip from './modules/tooltip.js'
+import initDropownMenu from './modules/dropdownmenu.js'
+import initMenuMobile from './modules/menu-mobile.js'
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTolltip();
+initDropownMenu();
+initMenuMobile();
